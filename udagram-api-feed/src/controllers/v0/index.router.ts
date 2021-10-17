50e0015b8bb4cb4895fcfd801af6eba846668a1e
Alan Corrales <alancorrales31@gmail.com>
2021-10-17 20:24:54 -0300

    added health routes

--- udagram-api-feed/src/controllers/v0/index.router.ts
+++ udagram-api-feed/src/controllers/v0/index.router.ts
@@ -3,10 +3,16 @@ import {FeedRouter} from './feed/routes/feed.router';
 
 const router: Router = Router();
 
+router.get('/feed/health', async (req: Request, res: Response) => {
+  res.sendStatus(200);
+});
+
 router.use('/feed', FeedRouter);
 
 router.get('/', async (req: Request, res: Response) => {
   res.send(`V0`);
 });
+
+
 
 export const IndexRouter: Router = router;
--- udagram-api-user/src/controllers/v0/index.router.ts
+++ udagram-api-user/src/controllers/v0/index.router.ts
@@ -3,6 +3,10 @@ import {UserRouter} from './users/routes/user.router';
 
 const router: Router = Router();
 
+router.get('/users/health', async (req: Request, res: Response) => {
+  res.sendStatus(200);
+});
+
 router.use('/users', UserRouter);
 
 router.get('/', async (req: Request, res: Response) => {
